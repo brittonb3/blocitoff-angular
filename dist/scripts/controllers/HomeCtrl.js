8846c14723870b5cfaccd483325f5e936e06c588
Britton Baumann <brittonbaumann@gmail.com>
2016-08-21 18:59:37 -0500

    checkpoint 3 add tasks

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -2,14 +2,14 @@
      function HomeCtrl($scope, $firebaseArray) {
          var ref = firebase.database().ref();
          $scope.tasks = $firebaseArray(ref);
-         
+
          $scope.addTask = function() {
              $scope.tasks.$add({
                  text: $scope.newTask
              });
          };
      }
- 
+
      angular
          .module('blocitoff')
          .controller('HomeCtrl', ['$scope', '$firebaseArray', HomeCtrl]);
