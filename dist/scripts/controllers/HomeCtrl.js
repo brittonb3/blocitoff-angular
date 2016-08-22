e097d2fce1f13b75fb8bc53b0a43caf18b59e5a1
Britton Baumann <brittonbaumann@gmail.com>
2016-08-22 00:08:41 -0500

    checkpoint 4 hide tasks

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -5,9 +5,21 @@
 
          $scope.addTask = function() {
              $scope.tasks.$add({
-                 text: $scope.newTask
+                 text: $scope.newTask,
+                 time: firebase.database.ServerValue.TIMESTAMP
              });
          };
+         
+         $scope.expiredTask = function(task) {
+            var currentTime = new Date();
+            if ((currentTime - task.time) >= 604800000){
+                return true;
+            } else {
+                return false;
+            }
+         };
+         
+         
      }
 
      angular
