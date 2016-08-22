03a8060658bb45be464e3397f1e2a923c4feebc9
Britton Baumann <brittonbaumann@gmail.com>
2016-08-22 12:02:06 -0500

    checkpoint 5 added history state, template, controller

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -11,7 +11,13 @@
              url: '/',
              controller: 'HomeCtrl as home',
              templateUrl: '/templates/home.html'
-         });
+         })
+         
+         .state('history', {
+             url: '/history',
+             controller: 'HistoryCtrl as history',
+             templateUrl: '/templates/history.html'
+        });
 
      }
  
